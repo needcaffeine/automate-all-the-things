ec571606a700037318ac3fa6ba7f98609883a6f7
Vic Vijayakumar <vic.vijayakumar@researchsquare.com>
2018-04-30 23:36:42 -0400

    fix: wait for form to fill it in
    Minor changes to how to fill in the login form, and how long we wait after
    login. This fixes #1.

--- kudos.js
+++ kudos.js
@@ -8,13 +8,14 @@ try {
         await page.setViewport({ width: 1280, height: 1024 })
         await page.goto('https://www.strava.com/')
         await page.click('a.btn-login')
-        await page.waitForNavigation()
+        await page.waitForSelector('form')
 
         // Login to the site.
-        await page.type('input[type=email]', process.env.STRAVA_EMAIL)
-        await page.type('input[type=password]', process.env.STRAVA_PASSWORD)
-        await page.click('#login-button')
+        await page.type('input#email', process.env.STRAVA_EMAIL)
+        await page.type('input#password', process.env.STRAVA_PASSWORD)
+        await page.click('button#login-button')
         await page.waitForNavigation({ waitUntil: 'networkidle2' });
+        await page.waitFor(3000)
 
         // Find and click all the unclicked kudos buttons.
         await page.$$eval('button[title="Give Kudos"]', buttons => {
